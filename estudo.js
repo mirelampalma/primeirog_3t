b04706c54b424c5cd0064e86c84197518604e00f
mirelampalma <108944879+mirelampalma@users.noreply.github.com>
2022-10-05 17:26:56 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,16 +1,36 @@
+let tabuada = 5;
 
-let tabuada = 2;
-
-
-function escreva("Tabuada do " + tabuada+ "<br>"){
-    document.write(tabuada+ " x 1 = "+(tabuada*1)+"<br>");
-    document.write(tabuada+ " x 1 = "+(tabuada*2)+"<br>");
-    document.write(tabuada+ " x 1 = "+(tabuada*3)+"<br>");
-    document.write(tabuada+ " x 1 = "+(tabuada*4)+"<br>");
-    document.write(tabuada+ " x 1 = "+(tabuada*5)+"<br>");
-    document.write(tabuada+ " x 1 = "+(tabuada*6)+"<br>");
-    document.write(tabuada+ " x 1 = "+(tabuada*7)+"<br>");
-    document.write(tabuada+ " x 1 = "+(tabuada*8)+"<br>");
-    document.write(tabuada+ " x 1 = "+(tabuada*9)+"<br>");
-    document.write(tabuada+ " x 1 = "+(tabuada*10)+"<br>");
+function escreva(){
+    document.write("Tabuada do " + "<br>");
+    document.write(tabuada+" x 1 = "+(tabuada*1)+"<br>");
+    document.write(tabuada+" x 2 = "+(tabuada*2)+"<br>");
+    document.write(tabuada+" x 3 = "+(tabuada*3)+"<br>");
+    document.write(tabuada+" x 4 = "+(tabuada*4)+"<br>");
+    document.write(tabuada+" x 5 = "+(tabuada*5)+"<br>");
+    document.write(tabuada+" x 6 = "+(tabuada*6)+"<br>");
+    document.write(tabuada+" x 7 = "+(tabuada*7)+"<br>");
+    document.write(tabuada+" x 8 = "+(tabuada*8)+"<br>");
+    document.write(tabuada+" x 9 = "+(tabuada*9)+"<br>");
+    document.write(tabuada+" x 10 = "+(tabuada*10)+"<br>");
 }
+
+let lista = ["Terra", "Marte", "Saturno", "Jupiter", "Vênus", "Mercurio"];
+
+function mostralista(){
+   document.write("Tamanho da lista: " + lista.length + "<br>");
+
+   for(let i = 0; i < lista.length; i++){
+    document.write("Planeta " + lista[i] + "<br>");
+   }
+    
+   }
+
+
+function repete(){
+    for(let i = 1; i <= 10; i++){
+        document.write("Valor do i = " + i + "<br>");
+        for(let j = 1; j <=10; j++)
+        document.write(i+" x " + j + " = "+(i*j)+"<br>");
+    }
+
+    }
