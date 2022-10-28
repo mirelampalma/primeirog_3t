0503ae29ba5fc9c03d13dcaf76f4b3f76a242107
mirelampalma <108944879+mirelampalma@users.noreply.github.com>
2022-10-28 12:45:46 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -23,8 +23,9 @@ function total(){
     let m = 0;
     for(let i = 1; i <= t; i++){
         m = c * (1 + (j/100));
-        document.write("Valor no mês " + i + " = " + m + "<br>");
+        //document.write("Valor no mês " + i + " = " + m + "<br>")
         c = m;
     }
-    document.write("Montante: " + m);
+    document.getElementById("total").innerHTML = m;
+    //document.write("Montante: " + m);
 }
